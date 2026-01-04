cfe811efe2d95ff842d83b9b1e7dbd8ceb1fe235
panos <panos@panosbampalis.com>
2026-01-04 14:12:24 +0200

    Fix dataset name

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'bnc1q8nk',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'puhk8qa7',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: '2024-01-01',
   useCdn: process.env.NODE_ENV === 'production',
